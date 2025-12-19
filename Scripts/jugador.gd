ce75a978bad5e0a7c62ce4be2eed766b98561d12
ShaimHowl <showlader@elpuig.xeill.net>
2025-12-19 12:12:42 +0100

    menu

--- Scripts/jugador.gd
+++ Scripts/jugador.gd
@@ -1,60 +1,43 @@
 extends CharacterBody2D
 
-@export var speed = 900
-@export var jump = -550
+@export var speed = 500
+@export var jump = -450
 
 var gravity = 980
-var attacking = false
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta):
 
+	# Gravedad
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	
-	if Input.is_action_just_pressed("Attack") and not attacking:
-		attacking = true
-		animated_sprite_2d.play("Attack")
-		 
-	
-	if Input.is_action_just_pressed("Attack2") and not attacking:
-		attacking = true
-		animated_sprite_2d.play("Attack2")
-
-	
-	if attacking:
-		if not animated_sprite_2d.is_playing():
-			attacking = false
-		pass
-
-	
+	# Salto
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump
-  
+
+	# Movimiento horizontal
 	var direction = Input.get_axis("move_left", "move_right")
 
-	
 	if direction < 0:
 		animated_sprite_2d.flip_h = true
 	elif direction > 0:
 		animated_sprite_2d.flip_h = false
 
-	
-	if not attacking:
-		if is_on_floor():
-			if direction == 0:
-				animated_sprite_2d.play("Idle")
-			else:
-				animated_sprite_2d.play("run")
+	# Animaciones
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("Idle")
 		else:
-			if velocity.y < 0:
-				animated_sprite_2d.play("jump")
-			else:
-				animated_sprite_2d.play("fall")
+			animated_sprite_2d.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite_2d.play("jump")
+		else:
+			animated_sprite_2d.play("fall")
 
-	
+	# Aplicar movimiento
 	if direction:
 		velocity.x = direction * speed
 	else:
