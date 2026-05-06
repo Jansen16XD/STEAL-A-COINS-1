01f854fb2a6c81282290f55af86555bfe801ae21
«ShaimHowl» <showlader@elpuig.xeill.net>
2026-05-06 12:34:42 +0200

    hecho

--- Scripts/mision_3.gd
+++ Scripts/mision_3.gd
@@ -1,13 +1,10 @@
 extends CanvasLayer
-
 @onready var titulo = $LabelTitulo
 @onready var texto = $LabelTexto
 @onready var sonido = $SonidoMision
 @onready var tecla_audio: AudioStreamPlayer = $Audio
-
 var titulo_completo := ""
 var texto_completo := ""
-
 var velocidad_titulo := 0.06
 var velocidad_texto := 0.04
 
@@ -17,27 +14,34 @@ func _ready():
 		sonido.play()
 	else:
 		titulo_completo = "MISIÓN FALLIDA"
-
+	
 	texto_completo = "Has cogido %d de %d monedas" % [
 		GameData.monedas,
 		GameData.monedas_totales
 	]
+	
+	titulo.text = ""
+	texto.text = ""
+	
+	await animar_titulo()
+	await animar_texto()
 
-	# Mostrar directamente sin animación
-	titulo.text = titulo_completo
-	texto.text = texto_completo
+func animar_titulo():
+	for i in titulo_completo.length():
+		titulo.text += titulo_completo[i]
+		await get_tree().create_timer(velocidad_titulo).timeout
 
-
-
+func animar_texto():
+	for i in texto_completo.length():
+		texto.text += texto_completo[i]
+		await get_tree().create_timer(velocidad_texto).timeout
 
 func _on_button_pressed() -> void:
 	tecla_audio.play()
 	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file("res://Scenes/detalles_4.tscn")
 
-
 func _on_button_3_pressed() -> void:
 	tecla_audio.play()
 	await get_tree().create_timer(0.1).timeout
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
-		 
--- Scripts/mision_6.gd
+++ Scripts/mision_6.gd
@@ -1,36 +1,53 @@
 extends CanvasLayer
-
 @onready var titulo = $LabelTitulo
 @onready var texto = $LabelTexto
+@onready var conseguido = $Conseguido
 @onready var sonido = $SonidoMision
 @onready var tecla_audio: AudioStreamPlayer = $Audio
-
 var titulo_completo := ""
 var texto_completo := ""
-
+var conseguido_completo := ""
 var velocidad_titulo := 0.06
 var velocidad_texto := 0.04
+var velocidad_conseguido := 0.05
 
 func _ready():
 	if GameData.monedas == GameData.monedas_totales:
 		titulo_completo = "MISIÓN COMPLETADA"
+		conseguido_completo = "¡Desafío completado!"
 		sonido.play()
 	else:
 		titulo_completo = "MISIÓN FALLIDA"
-
+		conseguido_completo = ""
+	
 	texto_completo = "Has cogido %d de %d monedas" % [
 		GameData.monedas,
 		GameData.monedas_totales
 	]
+	
+	titulo.text = ""
+	texto.text = ""
+	conseguido.text = ""
+	
+	await animar_titulo()
+	await animar_texto()
+	if conseguido_completo != "":
+		await animar_conseguido()
 
-	# Mostrar directamente sin animación
-	titulo.text = titulo_completo
-	texto.text = texto_completo
+func animar_titulo():
+	for i in titulo_completo.length():
+		titulo.text += titulo_completo[i]
+		await get_tree().create_timer(velocidad_titulo).timeout
 
+func animar_texto():
+	for i in texto_completo.length():
+		texto.text += texto_completo[i]
+		await get_tree().create_timer(velocidad_texto).timeout
 
-
-
-
+func animar_conseguido():
+	for i in conseguido_completo.length():
+		conseguido.text += conseguido_completo[i]
+		await get_tree().create_timer(velocidad_conseguido).timeout
 
 func _on_button_pressed() -> void:
 	tecla_audio.play()
